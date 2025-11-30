2569de147202d724172253d91f49a54508ef68f9
tikyydc <ervinaksizov@gmail.com>
2025-11-30 19:48:04 +0300

    car_showroom

---
+++ trade-app/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+db.sequelize.sync()
+.then(() => {
+ console.log("Synced db.");
+})
+.catch((err) => {
+ console.log("Failed to sync db: " + err.message);
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to trade-app application." });
+});
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
