eff2cdc424173d2dde640a74bf6dbfdf78f04433
tikyydc <ervinaksizov@gmail.com>
2025-12-15 23:40:41 +0300

    Added controllers and routes

--- trade-app/server.js
+++ trade-app/server.js
@@ -25,7 +25,19 @@ app.get("/", (req, res) => {
 });
 require("dotenv").config();
 
+
+
 const PORT = process.env.PORT || 8080;
+require("./app/routes/automobile.routes.js")(app);
+require("./app/routes/brand.routes.js")(app);
+require("./app/routes/client.routes.js")(app);
+require("./app/routes/delivery.routes.js")(app);
+require("./app/routes/model.routes.js")(app);
+require("./app/routes/order.routes.js")(app);
+require("./app/routes/order-item.routes.js")(app);
+require("./app/routes/payment.routes.js")(app);
+require("./app/routes/price-list.routes.js")(app);
+require("./app/routes/price-list-item.routes.js")(app);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
